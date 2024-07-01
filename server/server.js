cb7ed7547713dc536efd26a4260a77411327d34a
yousseflearn <youssefeasylearn@gmail.com>
2024-07-01 11:45:24 -0400

    Creation Backend functionality

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { config } from 'dotenv';
+
+const app = express();
+
+/** app middleware */
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+config();
+
+/** application port */
+const port = process.env.PORT || 8080;
+
+/** routes */
+app.get('/', (req, res) => {
+  try {
+    res.json('Get Request');
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is running on http:localhost:${port}`);
+});
